0a05aa0c1d845d4f7315c520102f7af7097adbf1
MN-Cool <c20334611@mytudublin.ie>
2022-12-02 10:05:34 +0000

    Updating Text Dialogue

---
+++ MichaelNoonanGiantHeadProject/Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    public Image actorImage;
+    public Text actorName;
+    public Text messageText;
+    public RectTransform dialogueBox;
+
+    Message[] currentMessages;
+    Actor[] currentActors;
+    int activeMessage = 0;
+    public static bool isActive = false;
+
+    public void OpenDialogue(Message[] messages, Actor[] actors)
+    {
+        currentMessages = messages;
+        currentActors = actors;
+        activeMessage = 0;
+        isActive = true;
+        DisplayDialogue();
+        dialogueBox.LeanScale(Vector3.one, 0.5f).setEaseInOutExpo();
+    }
+
+    void DisplayDialogue()
+    {
+        Message messageToDisplay = currentMessages[activeMessage];
+        messageText.text = messageToDisplay.message;
+
+        Actor actorToDisplay = currentActors[messageToDisplay.actorID];
+        actorName.text = actorToDisplay.name;
+        actorImage.sprite = actorToDisplay.sprite;
+
+        AnimateTextColor();
+    }
+
+    public void NextMessage()
+    {
+        activeMessage++;
+        if(activeMessage < currentMessages.Length)
+        {
+            DisplayDialogue();
+        }
+        else
+        {
+            isActive = false;
+            dialogueBox.LeanScale(Vector3.one, 0.5f).setEaseInOutExpo();
+        }
+    }
+
+    void AnimateTextColor()
+    {
+        LeanTween.textAlpha(messageText.rectTransform, 0, 0);
+        LeanTween.textAlpha(messageText.rectTransform, 1, 0.5f);
+    }
+
+    void Start()
+    {
+        dialogueBox.transform.localScale = Vector3.zero;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isActive == true)
+        {
+            NextMessage();
+        }
+    }
+}
--- MichaelNoonanGiantHeadProject/Assets/Scripts/FloatingHeadScript.cs
+++ MichaelNoonanGiantHeadProject/Assets/Scripts/FloatingHeadScript.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class FloatingHeadScript : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (DialogueManager.isActive == true)
+            return;
     }
 }
