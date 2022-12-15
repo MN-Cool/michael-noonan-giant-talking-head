b89c0c19794fb75b8ac9635eb6f4037b6097d6bc
MN-Cool <c20334611@mytudublin.ie>
2022-12-15 20:21:51 +0000

    Progress Update

--- MichaelNoonanGiantHeadProject/Assets/Scripts/NPCScript.cs
+++ MichaelNoonanGiantHeadProject/Assets/Scripts/NPCScript.cs
@@ -6,9 +6,29 @@ public class NPCScript : MonoBehaviour
 {
     public DialogueTrigger trigger;
 
-    private void OnCollisionEnter(Collision collision)
+    public Animator headAnimator;
+
+    void Start()
+    {
+        headAnimator = GetComponent<Animator>();
+    }
+
+    public void OnTriggerEnter(Collider collision)
     {
         if(collision.gameObject.CompareTag("Head") == true)
+        {
+            headAnimator.SetLayerWeight(headAnimator.GetLayerIndex("Turning Layer"), 1f);
             trigger.StartDialogue();
+        } 
+    }
+
+    public void HeadTalking()
+    {
+        headAnimator.SetLayerWeight(headAnimator.GetLayerIndex("Talking Layer"), 1f);
+    }
+
+    public void HeadStopTalking()
+    {
+        headAnimator.SetLayerWeight(headAnimator.GetLayerIndex("Talking Layer"), 0);
     }
 }
